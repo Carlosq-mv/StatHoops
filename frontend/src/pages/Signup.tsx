3e3ec3643237d4afdedf01df08b2d99ab7caa80e
Carlos Quiroz <cquiroz7869@gmail.com>
2024-05-17 17:19:50 -0700

    login & signup pages

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,18 +1,16 @@
-import AxiosInstance from '../components/Axios'
-import React, { useState } from 'react';
-import FormField from '../components/FormField';
-import IconButton from '@mui/material/IconButton';
+import React, { useState } from 'react'
+import { IconButton } from '@mui/material'
+import EmailIcon from '@mui/icons-material/Email';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import Button from '@mui/material/Button';
 import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
+import AxiosInstance from '../components/Axios';
+import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
+import FormField from '../components/FormField'
 import { Link } from 'react-router-dom';
 
-
-const Login = () => {
-  
+const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [formData, setFormData] = useState({
@@ -31,7 +29,7 @@ const Login = () => {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    AxiosInstance.post('/api/login/', formData)
+    AxiosInstance.post('/api/create-account/', formData)
     .then(response => {
       console.log(response.data);
       setFormData({
@@ -40,6 +38,7 @@ const Login = () => {
         username: ''
       });
       setErrorMessage('');
+      
     })
     .catch(error => {
       console.log(error.response.data);
@@ -48,25 +47,22 @@ const Login = () => {
 
       if(error.response.data.error)
         setErrorMessage(error.response.data.error);
-    })
+    });
   }
-
-
+  
   const handleClickShowPassword = () => setShowPassword((show) => !show);
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
   };
 
-
-
   return (
     <>
-      <div className="position-absolute top-50 start-50 translate-middle shadow-lg p-3 mb-5 bg-body-tertiary rounded">
-        <h1>Login</h1>
+      <div className=" position-absolute top-50 start-50 translate-middle shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+        <h1>Create Account</h1>
         <div className="col-sm-6 col-md-5 col-lg-6"></div>
 
         <div className="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0 ">
-          
+
           <form onSubmit={handleSubmit}>
               <div className="mb-3">
                 <FormField id="username" label="Username" value={formData.username} onChange={handleChange} startAdornment={<PersonIcon/>}/>
@@ -89,7 +85,7 @@ const Login = () => {
                   }/>
               </div>
 
-              <Button type="submit" variant="outlined">Login</Button>
+              <Button type="submit" variant="outlined">Signup</Button>
 
               <div className="mb-3">
                 {errorMessage && <Alert sx={{ m: 1, width: '35ch' }} severity="warning">{errorMessage}</Alert> }
@@ -97,12 +93,13 @@ const Login = () => {
           </form>
         </div>
 
-        <div className="text-center">
-            Don't Have an Account? <Link to="/signup">Sign Up</Link>
+        <div>
+            Login Here <Link to="/login">Login</Link>
         </div>
+
       </div>
     </>
   )
 }
 
-export default Login
+export default Signup
