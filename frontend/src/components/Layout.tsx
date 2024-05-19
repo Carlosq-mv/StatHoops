2a09ac88b009182482629475c81bbc810bd0413a
Carlos Quiroz <cquiroz7869@gmail.com>
2024-05-18 18:12:17 -0700

    layout component

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Sidebar from './Sidebar';
+import { useLocation } from 'react-router-dom';
+import { Box } from '@mui/material';
+import DarkMode from './DarkMode';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({children}: Props) => {
+  const location = useLocation();
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
+  return (
+    <>
+      <DarkMode>
+        <Box sx={{ display: 'flex' }}>
+          {!isAuthPage && <Sidebar />}
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, p: 3, width: isAuthPage ? '100%' : `calc(100% - 250px)` }}
+          > {children} </Box>
+        </Box>
+      </DarkMode>
+    </>
+  )
+}
+
+export default Layout
