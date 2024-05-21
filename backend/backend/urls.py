922da23b3d4445e0454f0e403f1a5fc05fe7f77e
Carlos Quiroz <cquiroz7869@gmail.com>
2024-05-20 23:58:55 -0700

    new urls mapping

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+
+    # User authentication Urls (Auth_app)
+    path('api/', include('auth_app.urls')),
+
+    # NBA Urls (Nba app)
+    path('nba/', include('nba.urls')),
+    
+    
+    # JWT Routes for Token access and refresh
+    path('api/token/', TokenObtainPairView.as_view(), name='get-token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
+    # REST Urls
+    path('api-auth/', include("rest_framework.urls")),
 ]
