b34286a405297fa843434b208707c8631bc18216
Carlos Quiroz <cquiroz7869@gmail.com>
2024-05-22 20:59:35 -0700

    serializer

--- backend/nba/serializers.py
+++ backend/nba/serializers.py
@@ -1,11 +1,13 @@
-from nba.models import Team
+from nba.models import Team, FranchiseLeader
 from rest_framework import serializers
-from django.contrib.auth.models import User
-
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['team_name', 'team_id', 'data_followed', 'user']
+        fields = ['team_name', 'team_id', 'user', 'city', 'state', 'year_founded', 'abbreviation', 'description']
         extra_kwargs = {'author' : {'read_only' : True}}
-        
+
+class FranchiseLeaderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FranchiseLeader
+        fields = ['TEAM_ID', 'PTS', 'PTS_PLAYER', 'AST', 'AST_PLAYER', 'REB', 'REB_PLAYER', 'BLK', 'BLK_PLAYER', 'STL', 'STL_PLAYER']
